5f8199dfbb30067c86e82b9d0972c9056159124b
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2024-10-19 22:05:43 +0700

    Update restore_snapshot.sh

--- allora-node/scripts/restore_snapshot.sh
+++ allora-node/scripts/restore_snapshot.sh
@@ -1,46 +1,28 @@
 #!/bin/bash
-set -eu
 
+set -euo pipefail  # Dừng script nếu có lỗi và bắt lỗi trong các pipeline
+
+# Định nghĩa màu sắc cho thông báo
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-NC='\033[0m'
+NC='\033[0m'  # No Color
 
+# Các biến liên quan đến khôi phục snapshot
 RESTORED_FLAG="${APP_HOME}/snapshot-restored.txt"
 S3_BUCKET="allora-edgenet-backups"
 LATEST_BACKUP_FILE_NAME="latest_backup.txt"
-RCLONE_S3_NAME="allora_s3" #! Replace with your rclone S3 name
+RCLONE_S3_NAME="allora_s3"  #! Thay thế bằng tên rclone S3 của bạn
 
-echo -e "Please ensure ${GREEN}allorad, rclone and zstd${NC} are installed and setup on your machine before running this script."
-echo -e "For rclone, please make sure to set ${GREEN}requester_pays: true${NC} in advance configuration"
-echo "After installing, re-run this script."
+LOGFILE="${APP_HOME}/restore.log"
 
-read -p "$(echo -e ${YELLOW}'Press [Enter] key to continue if dependencies are already installed... '${NC})"
+echo -e "Please ensure ${GREEN}allorad, rclone, and zstd${NC} are installed and configured on your machine before running this script."
+echo -e "For rclone, set ${GREEN}requester_pays: true${NC} in the advanced configuration."
+read -p "$(echo -e ${YELLOW}'Press [Enter] to continue if dependencies are installed... '${NC})"
 
+# Kiểm tra xem snapshot đã được khôi phục trước đó chưa
 if [ ! -f "$RESTORED_FLAG" ]; then
-  echo "Restoring the node from backup..."
+    echo "Restoring the node from backup..."
 
-  #* Define the latest archive and log file
-  LATEST_BACKUP_FILE=$(rclone cat "$RCLONE_S3_NAME:$S3_BUCKET/$LATEST_BACKUP_FILE_NAME")
-  LOGFILE="${APP_HOME}/restore.log"
-
-  #* Download the archive from S3 and extract it to the /data directory
-  mkdir -p "${APP_HOME}/data"
-  touch "$LOGFILE"
-  rm -rf "${APP_HOME}/data/*"
-  rclone -v cat "$RCLONE_S3_NAME:$S3_BUCKET/$LATEST_BACKUP_FILE" | tar --zstd -xvf - -C "${APP_HOME}/data" > "$LOGFILE" 2>&1
-  tail -n 50 "$LOGFILE"
-
-  echo "$LATEST_BACKUP_FILE" > "$RESTORED_FLAG"
-
-  # Get the user and group ID of the current user
-  USER_ID=$(id -u)
-  GROUP_ID=$(id -g)
-
-  # Change ownership to the current user
-  chown -R "$USER_ID:$GROUP_ID" "${APP_HOME}/data"
-else
-  RESTORED_SNAPSHOT=$(basename "$(cat "$RESTORED_FLAG")" .tar.zst)
-  echo -e "Node already restored with snapshot ${GREEN}$RESTORED_SNAPSHOT${NC}"
-  echo -e "${RED}To restore from the latest snapshot, remove the file: $RESTORED_FLAG${NC}"
-fi
+    # Lấy tên tệp snapshot mới nhất từ S3
+    LATES
