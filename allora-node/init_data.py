103602bb64e36bc8802f2943e10e15abb72a6848
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2024-10-19 21:56:21 +0700

    Update init_data.py

--- allora-node/init_data.py
+++ allora-node/init_data.py
@@ -2,24 +2,36 @@ import os
 from app_utils import init_price_token
 import sys
 
-# Initialize tokens at the start
 def initialize_tokens():
+    """
+    Hàm khởi tạo dữ liệu cho các token được chỉ định trong biến môi trường `TOKENS`.
+    Cấu trúc: 'ETH:ETHUSD,BTC:BTCUSD'
+    """
     try:
-        tokens = os.environ.get('TOKENS', '').split(',')
-        print(f"Tokens: {tokens}")
-        if tokens and len(tokens) > 0:
-            for token in tokens:
-                token_parts = token.split(':')
-                print(f"Token parts: {token_parts}")
-                if len(token_parts) == 2:
-                    token_name = f"{token_parts[0]}USD"
-                    print(f"Initializing data for {token_name} token")
-                    init_price_token(token_parts[0], token_name, 'usd')
+        tokens = os.getenv('TOKENS', '').split(',')
+        if not tokens or len(tokens) == 0:
+            print("No tokens specified in the TOKENS environment variable.")
+            sys.exit(1)
+
+        print(f"Tokens to initialize: {tokens}")
+
+        for token in tokens:
+            token_parts = token.split(':')
+            if len(token_parts) != 2:
+                print(f"Invalid token format: {token}. Expected format is <symbol>:<token_name>.")
+                continue
+
+            symbol, token_name = token_parts[0], token_parts[1]
+            print(f"Initializing data for {token_name} token.")
+            init_price_token(symbol, token_name, 'usd')
+
     except Exception as e:
         print(f"Failed to initialize tokens: {str(e)}")
+        sys.exit(1)
+    else:
+        print("Tokens initialized successfully.")
     finally:
-        print("Tokens initialized successfully")
         sys.exit(0)
 
-# Ensure tokens are initialized whether the script is run directly or through Gunicorn
-initialize_tokens()
+if __name__ == "__main__":
+    initialize_tokens()
