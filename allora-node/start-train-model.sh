842203556593caa62ed0d9a6cbd4737fb1160c91
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2024-10-20 13:03:54 +0700

    Update start-train-model.sh

--- allora-node/start-train-model.sh
+++ allora-node/start-train-model.sh
@@ -5,6 +5,11 @@
 
 docker build -f Dockerfile_train_models -t allora-train-model:1.0.0 .
 
+# Xóa container cũ nếu nó đã tồn tại để tránh xung đột
+if [ $(docker ps -a -q -f name=allora-train-model) ]; then
+  docker rm -f allora-train-model
+fi
+
 # Chạy Docker container để huấn luyện mô hình
 # Sử dụng GPU, chia sẻ thư mục dữ liệu và mô hình với container
 # Thiết lập biến môi trường DATABASE_PATH để trỏ đến cơ sở dữ liệu
